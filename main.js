c0b90f04cfdd4aca5c380af0f2c4fa70cd51386f
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-06 16:40:06 +0200

    Refactor HTML structure and enhance JavaScript form handling for train ticket calculation

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const form = document.getElementById("form");
-const fullNameOutput = document.querySelector("")
+const fullNameOutput = document.getElementById("fullNameOutput");
+const offerOutput = document.getElementById("offerOutput");
+const wagonOutput = document.getElementById("wagonOutput");
+const codeOutput = document.getElementById("codeOutput");
+const priceOutput = document.getElementById("priceOutput");
 
 
 form.addEventListener('submit', (event) => {
+    // Prevents page from reloading
     event.preventDefault();
+
+    // DOM Variables Declaration
     const inputfullName = document.getElementById("fullName").value;
     const inputKmNumber = document.getElementById("kmNumber").value;
     const inputAge = document.getElementById("age").value;
     let ticketPrice = inputKmNumber * 0.21;
     let offer = "Offerta Standard";
+    let wagon = Math.floor(Math.random() * 100);
+    let code = 91736;
+
+    // Condition for Different Ages
     if (inputAge < 18) {
         ticketPrice = ticketPrice - ((ticketPrice * 20) / 100);
         offer = "Offerta Minorenni";
@@ -18,5 +29,11 @@ form.addEventListener('submit', (event) => {
     } else {
         ticketPrice;
     }
-    console.log(`Grazie ${inputfullName}, ${offer}, Prezzo totale: ${ticketPrice.toLocaleString("it-IT", { style: "currency", currency: "EUR" })}`);
+
+    // Show Output
+    fullNameOutput.innerText = inputfullName;
+    offerOutput.innerText = offer;
+    wagonOutput.innerText = wagon;
+    codeOutput.innerText = code;
+    priceOutput.innerText = ticketPrice.toLocaleString("it-IT", { style: "currency", currency: "EUR" });
 })
